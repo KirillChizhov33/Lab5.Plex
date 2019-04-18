c03533f69d01315a8ffcd5478d96f18e7a5ce289
KirillChizhov33 <42999419+KirillChizhov33@users.noreply.github.com>
2019-04-18 22:12:15 +0300

    Version 0.0.7

--- MyForm.h
+++ MyForm.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #pragma once
 
 namespace ProjectUI {
@@ -8,18 +9,36 @@ namespace ProjectUI {
 	using namespace System::Windows::Forms;
 	using namespace System::Data;
 	using namespace System::Drawing;
-
-	#pragma once
+	
 	/// <summary>
 	/// Сводка для MyForm
 	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
 	public:
+		int x, y;
+		bool move;
+		TPoint *p_move;
+		TPoint *p1, *p2, *p3, *p4;
+		ChartPoint *sp1, *sp2, *sp3, *sp4;
+		ChartLine *l1, *l2, *l3;
+		Chart *s;
 		MyForm(void)
 		{
-			int x, y;
+			p1 = new TPoint(50, 50);
+			p2 = new TPoint(150, 50);
+			p3 = new TPoint(50, 150);
+			p4 = new TPoint(150, 150);
+			sp1 = new ChartPoint(p1);
+			sp2 = new ChartPoint(p2);
+			sp3 = new ChartPoint(p3);
+			sp4 = new ChartPoint(p4);
+			l1 = new ChartLine(sp1, sp2); 
+			l2 = new ChartLine(l1, sp3);
+			l3 = new ChartLine(l2, sp4);
+			s = l3;
 			InitializeComponent();
+			//move = false;
 			//
 			//TODO: добавьте код конструктора
 			//
@@ -36,7 +55,7 @@ namespace ProjectUI {
 				delete components;
 			}
 		}
-
+	private: System::Windows::Forms::Panel^  panel1;
 	protected:
 
 	private:
@@ -52,33 +71,76 @@ namespace ProjectUI {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->Location = System::Drawing::Point(27, 12);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(332, 125);
+			this->panel1->TabIndex = 0;
+			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::panel1_Paint);
+			this->panel1->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::panel1_MouseDown);
+			this->panel1->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::panel1_MouseMove);
+			this->panel1->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::panel1_MouseUp);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(384, 325);
+			this->ClientSize = System::Drawing::Size(394, 261);
+			this->Controls->Add(this->panel1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-		private: System::Void panel1_paint(System::Object ^ sender, System::Windows::Forms::PaintEventArgs^  e)
+	private: System::Void panel1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) 
 	{
 		Graphics ^g = e->Graphics;
 		Pen ^ p = gcnew Pen(Color::Red, 3);
-		Pen ^ c = gcnew Pen(Color::Blue, 3);
-		Pen ^ k = gcnew Pen(Color::Red, 3);
-		g->DrawLine(c, 50, 50, 120, 80);
-		g->DrawEllipse(p, 45, 45, 10, 10);
-		g->DrawEllipse(k, 115, 75, 10, 10);
+		s->paint(g);
+		if (p_move != NULL)
+		{
+			Pen ^ c = gcnew Pen(Color::Blue, 3);
+			g->DrawEllipse(c, p_move->x - 5, p_move->y - 5, 10, 10);
+		}
 	}
-
+	private: System::Void panel1_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) 
+	{
+		x = e->X;
+		y = e->Y;
+		panel1->Refresh();
+		move = ((e->X - 5 <= x) && (e->X + 5 >= x) && (e->Y - 5 <= y) && (e->Y + 5 >= y));
+		p_move = s->find(e->X, e->Y);
+	}
+	private: System::Void panel1_MouseMove(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) 
+	{
+		if (move)
+		{
+			x = e->X;
+			y = e->Y;
+		}
+		if ((move) || (p_move != NULL))
+		{
+			panel1->Refresh(); //Без этого условия есть только результат перемещения
+		}
+		if (p_move != NULL)
+		{
+			p_move->x = e->X; 
+			p_move->y = e->Y;
+		}
+	}
+	private: System::Void panel1_MouseUp(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) 
+	{
+		move = false;
+		p_move = NULL; //Не могу отпустить выбранную точку
+		panel1->Refresh(); 
+	}
 	};
-
-struct TPoint
+	struct TPoint
 	{
 		int x, y;
 		TPoint(int _x = 0, int _y = 0) :x(_x), y(_y){}
@@ -87,6 +149,7 @@ struct TPoint
 	{
 	public:
 		virtual TPoint * paint(Graphics ^g) = 0;
+		virtual TPoint * find(int _x, int _y) = 0;
 	};
 	class ChartPoint : public Chart
 	{
@@ -96,8 +159,16 @@ struct TPoint
 		virtual TPoint * paint(Graphics ^g)
 		{
 			Pen ^pn = gcnew Pen(Color::Red, 3);
-			g->DrawEllipse(pn, p->x - 5, p->y - 5, 10, 10); // координаты, ширина, высота 
+			g->DrawEllipse(pn, p->x - 5, p->y - 5, 10, 10); // координаты, ширина, высота
 			return p;
+		}
+		virtual TPoint * find(int _x, int _y)
+		{
+			if ((p->x - 5 <= _x) && (p->x + 5 >= _x) && (p->y - 5 <= _y) && (p->y + 5 >= _y))
+			{
+				return p;
+			}
+			else return NULL;
 		}
 	};
 	class ChartLine : public Chart
@@ -113,6 +184,15 @@ struct TPoint
 			g->DrawLine(pn, pl->x, pl->y, pr->x, pr->y); //координаты 2 точек
 			return pl;
 		}
+		virtual TPoint * find(int _x, int _y)
+		{
+			TPoint *pl = l->find(_x, _y);
+			TPoint *pr = r->find(_x, _y);
+			if (pl != NULL)
+			{
+				return pl;
+			}
+			return pr;
+		}
 	};
-
-} // alt+enter 
+}
