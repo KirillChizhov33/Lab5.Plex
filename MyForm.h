b34b3d1487e6230b45b6e7bb305e4ccffc96cfff
KirillChizhov33 <42999419+KirillChizhov33@users.noreply.github.com>
2019-04-24 19:32:07 +0300

    Version 0.1.0

--- MyForm.h
+++ MyForm.h
@@ -2,7 +2,7 @@
 #include "Plex.h"
 #pragma once
 
-namespace PlexProject {
+namespace Plexoid {
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -16,30 +16,35 @@ namespace PlexProject {
 	/// </summary>
 	public ref class MyForm : public System::Windows::Forms::Form
 	{
-		public:
+	public:
 		int x, y;
 		bool move;
 		TPoint *p_move;
-		TPoint *p1, *p2, *p3, *p4;
-		ChartPoint *sp1, *sp2, *sp3, *sp4;
-		ChartLine *l1, *l2, *l3;
+		TPoint *p1, *p2, *p3, *p4, *p5;
+		ChartPoint *sp1, *sp2, *sp3, *sp4, *sp5;
+		ChartLine *l1, *l2, *l3, *l4, *l5;
+	private: System::Windows::Forms::Button^  button2;
+	public:
 		Chart *s;
 		MyForm(void)
 		{
-			p1 = new TPoint(50, 50);
+			p1 = new TPoint(80, 50);
 			p2 = new TPoint(150, 50);
-			p3 = new TPoint(50, 150);
-			p4 = new TPoint(150, 150);
+			p3 = new TPoint(220, 50);
+			p4 = new TPoint(220, 200);
+			p5 = new TPoint(80, 200);
 			sp1 = new ChartPoint(p1);
 			sp2 = new ChartPoint(p2);
 			sp3 = new ChartPoint(p3);
 			sp4 = new ChartPoint(p4);
-			l1 = new ChartLine(sp1, sp2); 
-			l2 = new ChartLine(l1, sp3);
-			l3 = new ChartLine(l2, sp4);
-			s = l3;
+			sp5 = new ChartPoint(p5);
+			l1 = new ChartLine(sp2, sp3);
+			l2 = new ChartLine(l1, sp1);
+			l3 = new ChartLine(sp5, sp2);
+			l4 = new ChartLine(sp4, l3);
+			l5 = new ChartLine(l2, l4);
+			s = l5;
 			InitializeComponent();
-			//move = false;
 			//
 			//TODO: добавьте код конструктора
 			//
@@ -56,7 +61,7 @@ namespace PlexProject {
 			}
 		}
 	private: System::Windows::Forms::Panel^  panel1;
-	protected: 
+	protected:
 	private: System::Windows::Forms::Button^  button1;
 
 	private:
@@ -74,13 +79,14 @@ namespace PlexProject {
 		{
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// panel1
 			// 
 			this->panel1->Location = System::Drawing::Point(12, 12);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(459, 189);
+			this->panel1->Size = System::Drawing::Size(329, 277);
 			this->panel1->TabIndex = 0;
 			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::panel1_Paint);
 			this->panel1->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::panel1_MouseDown);
@@ -89,19 +95,30 @@ namespace PlexProject {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(377, 219);
+			this->button1->Location = System::Drawing::Point(363, 12);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(94, 70);
+			this->button1->Size = System::Drawing::Size(94, 49);
 			this->button1->TabIndex = 1;
 			this->button1->Text = L"Добавить точку";
 			this->button1->UseVisualStyleBackColor = true;
 			this->button1->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::button1_MouseClick);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(363, 67);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(94, 46);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"Удалить точку";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(483, 314);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->panel1);
 			this->Name = L"MyForm";
@@ -111,54 +128,62 @@ namespace PlexProject {
 		}
 #pragma endregion
 	private: System::Void panel1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e)
-			 {
-				 Graphics ^g = e->Graphics;
-				Pen ^ p = gcnew Pen(Color::Red, 3);
-				s->paint(g);
-				if (p_move != NULL)
-				{
-					Pen ^ c = gcnew Pen(Color::Blue, 3);
-					g->DrawEllipse(c, p_move->x - 5, p_move->y - 5, 10, 10);
-				}
-			 }
-	private: System::Void panel1_MouseUp(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) 
-			 {
-				 move = false;
-				p_move = NULL; //Не могу отпустить выбранную точку
-				panel1->Refresh(); 
-			 }
-	private: System::Void panel1_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) 
-			 {
-				 x = e->X;
-				y = e->Y;
-				panel1->Refresh();
-				move = ((e->X - 5 <= x) && (e->X + 5 >= x) && (e->Y - 5 <= y) && (e->Y + 5 >= y));
-				p_move = s->find(e->X, e->Y);
-			 }
-	private: System::Void panel1_MouseMove(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) 
-			 {
-				 if (move)
-				{
-					x = e->X;
-					y = e->Y;
-				}
-				if ((move) || (p_move != NULL))
-				{
-					panel1->Refresh(); //Без этого условия есть только результат перемещения
-				}
-				if (p_move != NULL)
-				{
-					p_move->x = e->X; 
-					p_move->y = e->Y;
-				}
-			 }
-	private: System::Void button1_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) 
-			 {
-				 TPoint *tmp;
-				ChartPoint *stmp;
-				tmp = new TPoint(100, 100);
-				stmp = new ChartPoint(tmp);
-				s = stmp;
-			 }
-	};
+	{
+		Graphics ^g = e->Graphics;
+		Pen ^ p = gcnew Pen(Color::Red, 3);
+		s->paint(g);
+		if (p_move != NULL)
+		{
+			Pen ^ c = gcnew Pen(Color::Blue, 3);
+			g->DrawEllipse(c, p_move->x - 5, p_move->y - 5, 10, 10);
+		}
+	}
+	private: System::Void panel1_MouseUp(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e)
+	{
+		move = false;
+		p_move = NULL; //Не могу отпустить выбранную точку
+		panel1->Refresh();
+	}
+	private: System::Void panel1_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e)
+	{
+		x = e->X;
+		y = e->Y;
+		panel1->Refresh();
+		move = ((e->X - 5 <= x) && (e->X + 5 >= x) && (e->Y - 5 <= y) && (e->Y + 5 >= y));
+		p_move = s->find(e->X, e->Y);
+	}
+	private: System::Void panel1_MouseMove(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e)
+	{
+		if (move)
+		{
+			x = e->X;
+			y = e->Y;
+		}
+		if ((move) || (p_move != NULL))
+		{
+			panel1->Refresh(); //Без этого условия есть только результат перемещения
+		}
+		if (p_move != NULL)
+		{
+			p_move->x = e->X;
+			p_move->y = e->Y;
+		}
+	}
+	private: System::Void button1_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e)
+	{
+		TPoint *tmp;
+		ChartPoint *stmp;
+		ChartLine *ltmp;
+		tmp = new TPoint(150, 150);
+		stmp = new ChartPoint(tmp);
+		ltmp = new ChartLine(l5, stmp);
+		s = ltmp;
+		panel1->Refresh();
+	}
+	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) 
+	{
+			s = l5;
+			panel1->Refresh();
+	}
+};
 }
