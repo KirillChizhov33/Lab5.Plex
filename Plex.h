74f529ea057d4139db514a4be07752d5f0120086
KirillChizhov33 <42999419+KirillChizhov33@users.noreply.github.com>
2019-05-09 22:47:58 +0300

    Version 0.0.2

--- Plex.h
+++ Plex.h
@@ -6,57 +6,72 @@ using namespace System::Windows::Forms;
 using namespace System::Data;
 using namespace System::Drawing;
 struct TPoint
+{
+	int x, y;
+	TPoint(int _x = 0, int _y = 0) :x(_x), y(_y){}
+};
+class Chart
+{
+public:
+	virtual TPoint * paint(Graphics ^g) = 0;
+	virtual TPoint * temps(Graphics ^g) = 0;
+	virtual TPoint * find(int _x, int _y) = 0;
+};
+class ChartPoint : public Chart
+{
+	TPoint * p;
+public:
+	ChartPoint(TPoint * _p) : p(_p){}
+	virtual TPoint * paint(Graphics ^g)
 	{
-		int x, y;
-		TPoint(int _x = 0, int _y = 0) :x(_x), y(_y){}
-	};
-	class Chart
+		Pen ^pn = gcnew Pen(Color::Red, 3);
+		g->DrawEllipse(pn, p->x - 5, p->y - 5, 10, 10); // координаты, ширина, высота
+		return p;
+	}
+	virtual TPoint * temps(Graphics ^g)
 	{
-	public:
-		virtual TPoint * paint(Graphics ^g) = 0;
-		virtual TPoint * find(int _x, int _y) = 0;
-	};
-	class ChartPoint : public Chart
+		Pen ^pn = gcnew Pen(Color::Red, 3);
+		g->DrawEllipse(pn, p->x - 5, p->y - 5, 10, 10); // координаты, ширина, высота
+		return p;
+	}
+	virtual TPoint * find(int _x, int _y)
 	{
-		TPoint * p;
-	public:
-		ChartPoint(TPoint * _p) : p(_p){}
-		virtual TPoint * paint(Graphics ^g)
+		if ((p->x - 5 <= _x) && (p->x + 5 >= _x) && (p->y - 5 <= _y) && (p->y + 5 >= _y))
 		{
-			Pen ^pn = gcnew Pen(Color::Red, 3);
-			g->DrawEllipse(pn, p->x - 5, p->y - 5, 10, 10); // координаты, ширина, высота
 			return p;
 		}
-		virtual TPoint * find(int _x, int _y)
-		{
-			if ((p->x - 5 <= _x) && (p->x + 5 >= _x) && (p->y - 5 <= _y) && (p->y + 5 >= _y))
-			{
-				return p;
-			}
-			else return NULL;
-		}
-	};
-	class ChartLine : public Chart
+		else return NULL;
+	}
+};
+class ChartLine : public Chart
+{
+	Chart *l, *r;
+public:
+	ChartLine(Chart *_l, Chart *_r) : r(_r), l(_l){}
+	virtual TPoint * paint(Graphics ^g)
 	{
-		Chart *l, *r;
-	public:
-		ChartLine(Chart *_l, Chart *_r) : r(_r), l(_l){}
-		virtual TPoint * paint(Graphics ^g)
+		Pen ^pn = gcnew Pen(Color::Green, 3);
+		TPoint *pl = l->paint(g);
+		TPoint *pr = r->paint(g);
+		g->DrawLine(pn, pl->x, pl->y, pr->x, pr->y); //координаты 2 точек
+		return pl;
+	}
+	virtual TPoint * temps(Graphics ^g)
+	{
+		Pen ^pn = gcnew Pen(Color::Yellow, 3);
+		TPoint *pl = l->paint(g);
+		TPoint *pr = r->paint(g);
+		g->DrawLine(pn, pl->x, pl->y, pr->x, pr->y); //координаты 2 точек
+		return pl;
+	}
+	virtual TPoint * find(int _x, int _y)
+	{
+		TPoint *pl = l->find(_x, _y);
+		TPoint *pr = r->find(_x, _y);
+		if (pl != NULL)
 		{
-			Pen ^pn = gcnew Pen(Color::Black, 3);
-			TPoint *pl = l->paint(g);
-			TPoint *pr = r->paint(g);
-			g->DrawLine(pn, pl->x, pl->y, pr->x, pr->y); //координаты 2 точек
 			return pl;
 		}
-		virtual TPoint * find(int _x, int _y)
-		{
-			TPoint *pl = l->find(_x, _y);
-			TPoint *pr = r->find(_x, _y);
-			if (pl != NULL)
-			{
-				return pl;
-			}
-			return pr;
-		}
-	};
+		return pr;
+	}
+};
