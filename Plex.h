48d30b4f8e940cce534125ab04d71482fc48ad55
KirillChizhov33 <42999419+KirillChizhov33@users.noreply.github.com>
2019-05-17 17:04:16 +0300

    Version 0.0.3

--- Plex.h
+++ Plex.h
@@ -5,16 +5,26 @@ using namespace System::Collections;
 using namespace System::Windows::Forms;
 using namespace System::Data;
 using namespace System::Drawing;
+
 struct TPoint
 {
 	int x, y;
 	TPoint(int _x = 0, int _y = 0) :x(_x), y(_y){}
+
+	String^ get(){
+		String^ s = gcnew String("");
+		s += "(";
+		s += x;
+		s += ",";
+		s += y;
+		s += ")";
+		return s;
+	}
 };
 class Chart
 {
 public:
 	virtual TPoint * paint(Graphics ^g) = 0;
-	virtual TPoint * temps(Graphics ^g) = 0;
 	virtual TPoint * find(int _x, int _y) = 0;
 };
 class ChartPoint : public Chart
@@ -23,12 +33,6 @@ class ChartPoint : public Chart
 public:
 	ChartPoint(TPoint * _p) : p(_p){}
 	virtual TPoint * paint(Graphics ^g)
-	{
-		Pen ^pn = gcnew Pen(Color::Red, 3);
-		g->DrawEllipse(pn, p->x - 5, p->y - 5, 10, 10); // координаты, ширина, высота
-		return p;
-	}
-	virtual TPoint * temps(Graphics ^g)
 	{
 		Pen ^pn = gcnew Pen(Color::Red, 3);
 		g->DrawEllipse(pn, p->x - 5, p->y - 5, 10, 10); // координаты, ширина, высота
@@ -51,14 +55,6 @@ public:
 	virtual TPoint * paint(Graphics ^g)
 	{
 		Pen ^pn = gcnew Pen(Color::Green, 3);
-		TPoint *pl = l->paint(g);
-		TPoint *pr = r->paint(g);
-		g->DrawLine(pn, pl->x, pl->y, pr->x, pr->y); //координаты 2 точек
-		return pl;
-	}
-	virtual TPoint * temps(Graphics ^g)
-	{
-		Pen ^pn = gcnew Pen(Color::Yellow, 3);
 		TPoint *pl = l->paint(g);
 		TPoint *pr = r->paint(g);
 		g->DrawLine(pn, pl->x, pl->y, pr->x, pr->y); //координаты 2 точек
